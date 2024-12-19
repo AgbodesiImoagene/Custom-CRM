1f2b92d4b285d68eaf62d3da809f000b6bea8c5d
Agbodesi Imoagene <agbodesii@gmail.com>
2024-12-19 00:24:51 +0000

    Updated root structure

---
+++ app/app.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.api.routes import router as api_router
+
+app = FastAPI()
+
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/ping")
+def ping():
+    return {"ping": "pong"}
+
+
+@app.get("/health")
+def health():
+    return {"status": "healthy"}
+
+
+app.include_router(api_router)
--- app/main.py
+++ app/main.py
@@ -1,31 +1,4 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
 
-from app.api.routes import router as api_router
-
-app = FastAPI()
-
-origins = [
-    "*",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
-@app.get("/ping")
-def ping():
-    return {"ping": "pong"}
-
-
-@app.get("/health")
-def health():
-    return {"status": "healthy"}
-
-
-app.include_router(api_router)
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="0.0.0.0", port=8000)
